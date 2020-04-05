41765734af3aa4d556d7774e5f864213075d6f2d
Ricardo Barros <1191202@isep.ipp.pt>
2020-04-05 20:01:56 +0100

    [Create a General Project Structure] changed folder names, changes class names, ref #1

---
+++ project/slaveNode/src/main/java/Result.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class Result {
+    public int value;
+    public int requestID;
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getRequestID() {
+        return requestID;
+    }
+
+
+    public void setRequestID(int requestID) {
+        this.requestID = requestID;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Result(int value, int requestID) {
+        this.value = value;
+        this.requestID = requestID;
+    }
+}

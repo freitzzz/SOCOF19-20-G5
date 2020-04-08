e9c1209ba0f9030e7a1e2dcb01848a8f3cfc14cc
João Freitas <1160907@isep.ipp.pt>
2020-04-08 19:11:48 +0100

    Implemented requestComputation for Lock Free Slave Handler

--- project/simulation/src/main/java/datastructures/handler/LockFreeSlaveHandler.java
+++ project/simulation/src/main/java/datastructures/handler/LockFreeSlaveHandler.java
@@ -7,6 +7,9 @@ import datastructures.map.LockFreeMap;
 import datastructures.scheduler.SlaveScheduler;
 import slave.Slave;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LockFreeSlaveHandler extends SlaveHandler {
 
     private final LockFreeList<Slave> slaves = new LockFreeList<>();
@@ -18,10 +21,12 @@ public class LockFreeSlaveHandler extends SlaveHandler {
     }
 
     @Override
-    public void requestComputation(Request request) {
-
-
-
+    public void requestComputation(final Request request) {
+        List<Slave> availableSlaves = slaves
+                .parallelStream()
+                .filter(this::tryReserveSlaveAvailability)
+                .collect(Collectors.toList());
+        super.scheduler.schedule(availableSlaves, request);
     }
 
     @Override
@@ -37,5 +42,17 @@ public class LockFreeSlaveHandler extends SlaveHandler {
     @Override
     public void reportAvailability(Slave slave, double availability) {
 
+    }
+
+    private boolean tryReserveSlaveAvailability(final Slave slave) {
+        boolean reserved = false;
+        final double currentSlaveAvailability  = slave.getAvailability().get();
+        final double slaveAvailabilityAfterCompute = currentSlaveAvailability - slave.getAvailabilityReducePerCompute();
+        if(slaveAvailabilityAfterCompute >= 0d) {
+
+            reserved = slave.getAvailability().compareAndSet(currentSlaveAvailability, slaveAvailabilityAfterCompute);
+
+        }
+        return reserved;
     }
 }

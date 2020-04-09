5fc27221f91b4e08b2791cd5b261e48b6c6f68ab
João Freitas <1160907@isep.ipp.pt>
2020-04-09 13:36:39 +0100

    Added Master class with receiveResult and receiveRequestCouldNotBeScheduled methods

---
+++ project/simulation/src/main/java/master/Master.java
@@ -0,0 +1,20 @@
+package master;
+
+import datastructures.Request;
+import datastructures.Result;
+
+public class Master {
+
+    public void receiveResult(final Result result) {
+
+        throw new UnsupportedOperationException("still not implemented");
+
+    }
+
+    public void receiveRequestCouldNotBeScheduled(final Request request) {
+
+        throw new UnsupportedOperationException("still not implemented");
+
+    }
+
+}

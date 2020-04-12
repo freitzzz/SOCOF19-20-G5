41683f7030ebc8f118ff38185754e020f068e2e1
Ricardo Barros <1191202@isep.ipp.pt>
2020-04-12 09:12:59 +0100

    [Implement Lock Mechanism] updated lock based unit tests, ref #3

--- project/simulation/src/test/java/datastructures/handler/LockBasedSlaveHandlerTest.java
+++ project/simulation/src/test/java/datastructures/handler/LockBasedSlaveHandlerTest.java
@@ -1,6 +1,7 @@
 package datastructures.handler;
 
-import datastructures.CodeExecutionRequest;
+import datastructures.*;
+import datastructures.scheduler.PerformanceIndexSlaveScheduler;
 import datastructures.scheduler.SlaveScheduler;
 import org.junit.After;
 import org.junit.BeforeClass;
@@ -20,6 +21,8 @@ public class LockBasedSlaveHandlerTest {
     private static final SlaveScheduler scheduler = mock(SlaveScheduler.class);
 
     private static final CodeExecutionRequest codeExecutionRequest = mock(CodeExecutionRequest.class);
+
+    private static final ReportPerformanceIndexRequest reportPerformanceIndexRequest = mock(ReportPerformanceIndexRequest.class);
 
     private static final Master master = mock(Master.class);
 
@@ -42,6 +45,10 @@ public class LockBasedSlaveHandlerTest {
         reset(scheduler);
 
         reset(codeExecutionRequest);
+
+        reset(reportPerformanceIndexRequest);
+
+        reset(master);
     }
     @Test
     public void ensureAllSlavesAreScheduledForComputeIfAllAllowAvailabilityReserveOnRequestComputation() {
@@ -106,6 +113,213 @@ public class LockBasedSlaveHandlerTest {
         verify(scheduler, only()).schedule(expectedSlavesScheduledForCompute, codeExecutionRequest, slaveHandler);
 
         verify(master, never()).receiveRequestCouldNotBeScheduled(codeExecutionRequest);
+    }
+
+    @Test
+    public void ensureNoSlavesAreNotScheduledForComputeIfNoSlaveIsAvailableToBeReserved() {
+
+        slaves.forEach(slave -> {
+            when(slave.getAvailability()).thenReturn(new AtomicInteger(20));
+            when(slave.getAvailabilityReducePerCompute(codeExecutionRequest)).thenReturn(25);
+        });
+
+        SlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        slaveHandler.requestSlaves(codeExecutionRequest);
+
+        slaves.forEach(slave -> {
+            verify(slave, times(1)).getAvailability();
+            verify(slave, times(1)).getAvailabilityReducePerCompute(codeExecutionRequest);
+        });
+
+        verify(scheduler, never()).schedule(any(List.class), any(CodeExecutionRequest.class), any(SlaveHandler.class));
+
+        verify(master, only()).receiveRequestCouldNotBeScheduled(codeExecutionRequest);
+    }
+    @Test
+    public void ensureSlavesReportPerformanceIndexIfRequestIsReportPerformanceIndexRequest() {
+
+        slaves.forEach(slave -> {
+            when(slave.getPerformanceIndex()).thenReturn(1);
+            when(slave.getAvailabilityReducePerCompute(reportPerformanceIndexRequest)).thenReturn(0);
+            when(slave.getAvailability()).thenReturn(new AtomicInteger(100));
+        });
+
+        SlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        slaveHandler.requestSlaves(reportPerformanceIndexRequest);
+
+        slaves.forEach(slave -> verify(slave, atMostOnce()).process(reportPerformanceIndexRequest, slaveHandler));
+
+        verify(scheduler, never()).schedule(anyList(), any(CodeExecutionRequest.class), any(SlaveHandler.class));
+
+    }
+
+    @Test
+    public void ensureWhenPushingResultsMasterDoesNotReceiveTheFinalResultIfScheduledSlavesHaventFinishCompute() {
+
+        LockBasedSlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        slaves.forEach(slave -> {
+            when(slave.getAvailability()).thenReturn(new AtomicInteger(100));
+            when(slave.getAvailabilityReducePerCompute(codeExecutionRequest)).thenReturn(25);
+        });
+
+        when(codeExecutionRequest.getRequestID()).thenReturn(1);
+
+        slaveHandler.requestSlaves(codeExecutionRequest);
+
+        Result result = mock(Result.class);
+
+        when(result.getRequestID()).thenReturn(1);
+
+        // don't push the result of the last slave
+
+        for(int i = 0; i < slaves.size() - 1; i++) {
+            slaveHandler.pushResult(result);
+        }
+
+        verify(result, times(slaves.size() - 1)).getRequestID();
+
+        verify(master, never()).receiveResult(any(Result.class));
+
+        // now push the last one
+
+        slaveHandler.pushResult(result);
+
+        verify(result, times(slaves.size() + 2)).getRequestID();
+
+        verify(result, times(slaves.size())).getValue();
+
+        verify(master, only()).receiveResult(any(Result.class));
+    }
+
+    @Test
+    public void ensureMasterReceivesTheCorrectValueOfTheFinalResult() {
+
+        LockBasedSlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        slaves.forEach(slave -> {
+            when(slave.getAvailability()).thenReturn(new AtomicInteger(100));
+            when(slave.getAvailabilityReducePerCompute(codeExecutionRequest)).thenReturn(25);
+        });
+
+        when(codeExecutionRequest.getRequestID()).thenReturn(1);
+
+        slaveHandler.requestSlaves(codeExecutionRequest);
+
+        Result result = mock(Result.class);
+
+        when(result.getRequestID()).thenReturn(1);
+
+        when(result.getValue()).thenReturn(1);
+
+        // don't push the result of the last slave
+
+        for(int i = 0; i < slaves.size(); i++) {
+            slaveHandler.pushResult(result);
+        }
+
+        Result finalResult = new Result(slaves.size() * 1, 1);
+
+        verify(master, only()).receiveResult(finalResult);
+    }
+
+    @Test
+    public void ensureMasterReceivesSlavePerformanceDetails() {
+
+        Slave slave = slaves.get(0);
+
+        when(slave.getPerformanceIndex()).thenReturn(1);
+
+        SlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        PerformanceDetails expectedPerformanceDetails = new PerformanceDetails(slave, slave.getPerformanceIndex());
+
+        slaveHandler.reportPerformance(slave);
+
+        verify(master, only()).receiveSlavePerformanceDetails(expectedPerformanceDetails);
+
+    }
+
+    @Test
+    public void ensureMasterReceivesSlaveAvailabilityDetails() {
+
+        Slave slave = slaves.get(0);
+
+        when(slave.getAvailability()).thenReturn(new AtomicInteger(1));
+
+        SlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        AvailabilityDetails expectedAvailabilityDetails = new AvailabilityDetails(slave, slave.getAvailability().intValue());
+
+        slaveHandler.reportAvailability(slave, codeExecutionRequest);
+
+        verify(master, only()).receiveSlaveAvailability(expectedAvailabilityDetails);
+
+    }
+
+    @Test
+    public void ensureSlaveHandlerHandlesConcurrentCallsAsExpected() {
+
+        List<Slave> slaves = new ArrayList<>();
+
+        Master master = spy(Master.class);
+
+        SlaveScheduler scheduler = new PerformanceIndexSlaveScheduler();
+
+        for(int i = 0; i < 5; i++) {
+
+            slaves.add(new Slave(i + 1));
+
+        }
+
+        LockBasedSlaveHandler slaveHandler = new LockBasedSlaveHandler(scheduler, master, slaves);
+
+        final List<Integer> values = new ArrayList<>();
+
+        for(int i = 0; i < 100000; i++) {
+            values.add(i);
+        }
+
+        final int expectedSum = values.parallelStream().reduce(0, (integer, integer2) -> integer + integer2);
+
+        doAnswer(invocation -> {
+
+            slaveHandler.requestSlaves(invocation.getArgument(0));
+
+            return null;
+        }).when(master).receiveRequestCouldNotBeScheduled(any(Request.class));
+
+        doAnswer(invocation -> {
+
+            return null;
+        }).when(master).receiveSlaveAvailability(any(AvailabilityDetails.class));
+
+        final AtomicInteger timesInvoked = new AtomicInteger();
+
+        doAnswer(invocation -> {
+            timesInvoked.getAndIncrement();
+            Result result = invocation.getArgument(0);
+            if(result.getValue() != expectedSum) {
+                throw new Exception();
+            }
+            return null;
+        }).when(master).receiveResult(any(Result.class));
+
+        for(int i = 0; i < 10000; i++) {
+            slaveHandler.requestSlaves(new CodeExecutionRequest(values, i, CodeExecutionRequest.Operation.ADD));
+        }
+
+        ((Runnable) () -> {
+            while (timesInvoked.intValue() < 10000) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }).run();
     }
 
 
